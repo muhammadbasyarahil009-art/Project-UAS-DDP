4bb1c58faf589f67b0a5e5c74ddc476d4b71f5aa
Muhammad Basyarahil <muhammadbasyarahil009@gmail.com>
2026-01-16 16:08:05 +0700

    Update codingan terbaru

--- store/views.py
+++ store/views.py
@@ -10,7 +10,6 @@ def home(request):
     low_stock = materials.filter(stock__lt=10).count()
     total_asset = sum(item.price * item.stock for item in materials)
 
-    # Membungkus semua data ke dalam variable 'context'
     context = {
         'materials': materials,
         'total_material': total_material,
@@ -26,7 +25,6 @@ def detail(request, material_id):
     material = get_object_or_404(Material, pk=material_id)
     return render(request, 'store/detail.html', {'material': material})
 
-# ... (kode yang sudah ada sebelumnya biarkan saja)
 
 def about(request):
     return render(request, 'store/about.html')
@@ -41,13 +39,13 @@ def employee(request):
     context = {}
 
     if request.method == 'POST':
-        # 1. Ambil data dari form HTML
+        # Ambil data dari form HTML
         nama = request.POST.get('nama')
         jabatan = request.POST.get('jabatan')
         agama = request.POST.get('agama')
         status = request.POST.get('status')
 
-        # 2. Tentukan Gaji Pokok berdasarkan Jabatan (Simulasi)
+        # Tentukan Gaji Pokok berdasarkan Jabatan (Simulasi)
         gaji_pokok = 0
         if jabatan == 'Manager':
             gaji_pokok = 20000000
@@ -62,23 +60,23 @@ def employee(request):
         elif jabatan == 'Kasir':
             gaji_pokok = 7500000
         
-        # 3. Hitung Tunjangan (Jika Menikah tambah 1 Juta)
+        # Hitung Tunjangan (Jika Menikah tambah 1 Juta)
         tunjangan = 0
         if status == 'Menikah':
             tunjangan = 1000000
         
-        # 4. Hitung Gaji Kotor
+        # Hitung Gaji Kotor
         gaji_kotor = gaji_pokok + tunjangan
 
-        # 5. Hitung Zakat (Jika Islam, potong 2.5%)
+        # Hitung Zakat (Jika Islam, potong 2.5%)
         zakat = 0
         if agama == 'Islam' and gaji_kotor >= 0:
             zakat = gaji_kotor * 0.025 # 2.5 persen
         
-        # 6. Hitung Gaji Bersih (Take Home Pay)
+        # Hitung Gaji Bersih (Take Home Pay)
         gaji_bersih = gaji_kotor - zakat
 
-        # 7. Bungkus data untuk dikirim balik ke HTML
+        # Bungkus data untuk dikirim balik ke HTML
         context = {
             'hasil': True,
             'nama': nama,
